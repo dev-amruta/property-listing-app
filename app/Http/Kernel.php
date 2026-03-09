0effbe4f4f90ccb6fc8b75379e3571f89868fa08
dev-amruta <123798187+dev-amruta@users.noreply.github.com>
2026-03-09 16:31:48 +0530

    Add HTTP Kernel with middleware configuration

---
+++ app/Http/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ]
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    ];
+}
